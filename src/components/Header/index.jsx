183516d24245d4945d61ace8ff529d2ec62ba03a
Laiane Suzart <laiane@pm.me>
2021-05-26 10:16:53 -0300

    feat: user can visualize how many items on their bag

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,32 +1,45 @@
-import { AppBar, Toolbar, Fab, Tooltip } from "@material-ui/core";
-import {useHistory} from 'react-router-dom';
+import { AppBar, Toolbar, Fab, Tooltip, Badge } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
 import logo from "../../assets/logo.png";
 import LocalMallIcon from "@material-ui/icons/LocalMall";
 import HomeIcon from "@material-ui/icons/Home";
 
 function Header({ isHome = false }) {
-const history = useHistory()
+  const history = useHistory();
+  const { cart } = useSelector((store) => store);
 
-function redirectTo(path) {
-    history.push(path)
-}
+  function redirectTo(path) {
+    history.push(path);
+  }
+  
   return (
     <AppBar position="static">
-      <Toolbar style={{backgroundColor: 'lightpink'}}>
-        <img src={logo} alt="Logo Candy Shop" style={{ maxWidth: "50%", marginRight: 'auto' }}/>
-      {isHome ? (
-        <Tooltip title="Bolsa">
-          <Fab color="secondary" size="small" onClick={() => redirectTo("/cart")}>
-            <LocalMallIcon />
-          </Fab>
-        </Tooltip>
-      ) : (
-        <Tooltip title="Início">
-          <Fab color="secondary" size="small" onClick={() => redirectTo("/")}>
-            <HomeIcon />
-          </Fab>
-        </Tooltip>
-      )}
+      <Toolbar style={{ backgroundColor: "lightpink" }}>
+        <img
+          src={logo}
+          alt="Logo Candy Shop"
+          style={{ maxWidth: "50%", marginRight: "auto" }}
+        />
+        {isHome ? (
+          <Tooltip title="Bolsa">
+            <Fab
+              color="secondary"
+              size="small"
+              onClick={() => redirectTo("/cart")}
+            >
+              <Badge badgeContent={cart.length} color="secondary">
+                <LocalMallIcon />
+              </Badge>
+            </Fab>
+          </Tooltip>
+        ) : (
+          <Tooltip title="Início">
+            <Fab color="secondary" size="small" onClick={() => redirectTo("/")}>
+              <HomeIcon />
+            </Fab>
+          </Tooltip>
+        )}
       </Toolbar>
     </AppBar>
   );
--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,15 +1,15 @@
 import { useSelector } from "react-redux";
+import { EmptyCart } from "./style";
 import Header from "../../components/Header";
 import Title from "../../components/Title";
 import CardContainer from "../../components/CardContainer";
 import Card from "../../components/Card";
-import { EmptyCart } from "./style";
 
 function Cart() {
   const { cart } = useSelector((store) => store);
   return (
     <div>
-      <Header />
+      <Header/>
       <Title>Minha Bolsa</Title>
       {cart.length > 0 ? (
         <CardContainer>
